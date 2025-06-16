bb6cabc230695616a4ab193d2374e73ef572f5c9
nathanielfaiad <nathaniel.jon.faiad@gmail.com>
2025-06-16 09:36:56 -0500

    FunctionalMapper continued

--- src/main/java/com/filesplitter/FileSplitter/factory/ColumnMapperFactory.java
+++ src/main/java/com/filesplitter/FileSplitter/factory/ColumnMapperFactory.java
@@ -2,15 +2,12 @@ package com.filesplitter.FileSplitter.factory;
 
 import com.filesplitter.FileSplitter.constants.JudiColumn;
 import com.filesplitter.FileSplitter.constants.PrimeColumn;
-import com.filesplitter.FileSplitter.mapper.ColumnMapper;
 import com.filesplitter.FileSplitter.mapper.FunctionalMapper;
 import com.filesplitter.FileSplitter.mapper.OutputMapping;
-import com.filesplitter.FileSplitter.mapper.PairMapper;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 
 public class ColumnMapperFactory {
 
@@ -35,40 +32,20 @@ public class ColumnMapperFactory {
       FunctionalMapper.constantMapper(PrimeColumn.MEDICAL_OR_PHARMACY.getIndex(), "P")
   );
 
-  private static final List<ColumnMapper> ALT_FALSE_COLUMN_MAPPERS = getColumnMappersForFileType(false);
+  private static final List<FunctionalMapper> ALT_FALSE_COLUMN_MAPPERS = getFunctionalMappersForFileType(false);
 
-  private static final List<ColumnMapper> ALT_TRUE_COLUMN_MAPPERS = getColumnMappersForFileType(true);
-
-  private static final Function<String, String> BASE_AMOUNT_FUNCTION = baseValue -> {
-    // Evaluate the base value here
-    try {
-      double number = Double.parseDouble(baseValue);
-      return String.valueOf(Math.abs(number));
-    } catch (NumberFormatException e) {
-      return null;
-    }
-  };
-
-  public static List<ColumnMapper> getColumnMappers(boolean isAltFile) {
-    return isAltFile ? ALT_TRUE_COLUMN_MAPPERS : ALT_FALSE_COLUMN_MAPPERS;
-  }
+  private static final List<FunctionalMapper> ALT_TRUE_COLUMN_MAPPERS = getFunctionalMappersForFileType(true);
 
   public static List<FunctionalMapper> getFunctionalMappers(boolean isAltFile) {
-
-    return List.of();
-  }
-
-  private static List<ColumnMapper> getColumnMappersForFileType(boolean isAltFile) {
-    List<ColumnMapper> columnMappers = new ArrayList<>();
-    columnMappers.addAll(getDeductiblePairMappers(isAltFile));
-    columnMappers.addAll(getOopPairMappers(isAltFile));
-    return Collections.unmodifiableList(columnMappers);
+    return isAltFile ? ALT_TRUE_COLUMN_MAPPERS : ALT_FALSE_COLUMN_MAPPERS;
   }
 
   private static List<FunctionalMapper> getFunctionalMappersForFileType(boolean isAltFile) {
     List<FunctionalMapper> columnMappers = new ArrayList<>(CONSTANT_MAPPERS);
     columnMappers.addAll(COMMON_MAPPERS);
     columnMappers.addAll(getConstantFunctionalMappers(isAltFile));
+    columnMappers.addAll(getDeductibleMappers(isAltFile));
+    columnMappers.addAll(getOutOfPocketMappers(isAltFile));
     columnMappers.addAll(getConcatMappers());
     return Collections.unmodifiableList(columnMappers);
   }
@@ -92,110 +69,69 @@ public class ColumnMapperFactory {
     ));
   }
 
-  //  private static List<ConditionalMapper> getDeductibleMappers(boolean isAltFile) {
-  //    int amountColumnIndex = isAltFile ? PrimeColumn.DEDUCTIBLE_ALTERNATE_DOLLAR_AMOUNT_NEEDED_FROM_PRIME.getIndex()
-  //        : PrimeColumn.DEDUCTIBLE_REAL_DOLLAR_AMOUNT_NEEDED_FROM_PRIME.getIndex();
-  //
-  //    ConditionalTarget[] commonTargets = new ConditionalTarget[]{new ConditionalTarget(
-  //        PrimeColumn.DEDUCTIBLE_ACCUM_CODE.getIndex(),
-  //        (row, baseValue) -> baseValue != null && Double.parseDouble(baseValue) > 0 ? "P" : "X"
-  //    ), new ConditionalTarget(
-  //        PrimeColumn.DEDUCTIBLE_ADJUSTMENT_REASON_CODE.getIndex(),
-  //        (row, baseValue) -> baseValue != null ? "JUDISEED" : null
-  //    )};
-  //
-  //    return List.of(new ConditionalMapper(
-  //        amountColumnIndex,
-  //        BASE_AMOUNT_FUNCTION,
-  //        commonTargets
-  //    ));
-  //  }
-
   private static List<FunctionalMapper> getDeductibleMappers(boolean isAltFile) {
     int amountColumnIndex = isAltFile ? PrimeColumn.DEDUCTIBLE_ALTERNATE_DOLLAR_AMOUNT_NEEDED_FROM_PRIME.getIndex()
         : PrimeColumn.DEDUCTIBLE_REAL_DOLLAR_AMOUNT_NEEDED_FROM_PRIME.getIndex();
 
     return List.of(
         new FunctionalMapper(row -> {
-          Optional<List<OutputMapping>> optionalList = getAmountMapping(
-              row,
-              JudiColumn.DED_BOTH.getIndex(),
-              JudiColumn.DED_BOTH_AMOUNT.getIndex(),
-              PrimeColumn.DEDUCTIBLE_ACCUM_CODE.getIndex(),
-              amountColumnIndex
-          );
-          if (optionalList.isPresent()) {
-            return optionalList.get();
+          int[][] inputIndexesArray = new int[][]{
+              {JudiColumn.DED_BOTH.getIndex(), JudiColumn.DED_BOTH_AMOUNT.getIndex()},
+              {JudiColumn.DED_OON.getIndex(), JudiColumn.DED_OON_AMOUNT.getIndex()},
+              {JudiColumn.DED_ONC.getIndex(), JudiColumn.DED_ONC_AMOUNT.getIndex()},
+              {JudiColumn.DED_ONC_OON.getIndex(), JudiColumn.DED_ONC_OON_AMOUNT.getIndex()},
+              {JudiColumn.DED_SPEC.getIndex(), JudiColumn.DED_SPEC_AMOUNT.getIndex()}
+          };
+
+          for (int[] inputIndexes : inputIndexesArray) {
+            Optional<List<OutputMapping>> optionalList = getAmountMapping(
+                row,
+                inputIndexes[0], // inputCodeIndex
+                inputIndexes[1], // inputAmountIndex
+                PrimeColumn.DEDUCTIBLE_ACCUM_CODE.getIndex(),
+                amountColumnIndex
+            );
+            if (optionalList.isPresent()) {
+              return optionalList.get();
+            }
           }
 
-          Optional<List<OutputMapping>> optionalList2 = getAmountMapping(
-              row,
-              JudiColumn.DED_OON.getIndex(),
-              JudiColumn.DED_OON_AMOUNT.getIndex(),
-              PrimeColumn.DEDUCTIBLE_ACCUM_CODE.getIndex(),
-              amountColumnIndex
-          );
-          if (optionalList2.isPresent()) {
-            return optionalList2.get();
-          }
-
-          // TODO: Complete the rest and create another method
+          // Default to nothing if a non-zero value is not found
           return Collections.emptyList();
         })
     );
   }
 
-  private static List<PairMapper> getDeductiblePairMappers(boolean isAltFile) {
-    int amountColumnIndex = isAltFile ? PrimeColumn.DEDUCTIBLE_ALTERNATE_DOLLAR_AMOUNT_NEEDED_FROM_PRIME.getIndex()
-        : PrimeColumn.DEDUCTIBLE_REAL_DOLLAR_AMOUNT_NEEDED_FROM_PRIME.getIndex();
-    return List.of(
-        new PairMapper(
-            new int[]{JudiColumn.DED_ONC.getIndex(), JudiColumn.DED_ONC_AMOUNT.getIndex()},
-            PrimeColumn.DEDUCTIBLE_ACCUM_CODE.getIndex(),
-            amountColumnIndex
-        ),
-        new PairMapper(
-            new int[]{JudiColumn.DED_ONC_OON.getIndex(), JudiColumn.DED_ONC_OON_AMOUNT.getIndex()},
-            PrimeColumn.DEDUCTIBLE_ACCUM_CODE.getIndex(),
-            amountColumnIndex
-        ),
-        new PairMapper(
-            new int[]{JudiColumn.DED_SPEC.getIndex(), JudiColumn.DED_SPEC_AMOUNT.getIndex()},
-            PrimeColumn.DEDUCTIBLE_ACCUM_CODE.getIndex(),
-            amountColumnIndex
-        )
-    );
-  }
-
-  private static List<PairMapper> getOopPairMappers(boolean isAltFile) {
+  private static List<FunctionalMapper> getOutOfPocketMappers(boolean isAltFile) {
     int amountColumnIndex = isAltFile ? PrimeColumn.OUT_OF_POCKET_ALTERNATE_DOLLAR_AMOUNT_NEEDED_FROM_PRIME.getIndex()
         : PrimeColumn.OUT_OF_POCKET_REAL_DOLLAR_AMOUNT_NEEDED_FROM_PRIME.getIndex();
+
     return List.of(
-        new PairMapper(
-            new int[]{JudiColumn.OOP_BOTH.getIndex(), JudiColumn.OOP_BOTH_AMOUNT.getIndex()},
-            PrimeColumn.OUT_OF_POCKET_ACCUM_CODE.getIndex(),
-            amountColumnIndex
-        ),
-        new PairMapper(
-            new int[]{JudiColumn.OOP_OON.getIndex(), JudiColumn.OOP_OON_AMOUNT.getIndex()},
-            PrimeColumn.OUT_OF_POCKET_ACCUM_CODE.getIndex(),
-            amountColumnIndex
-        ),
-        new PairMapper(
-            new int[]{JudiColumn.OOP_ONC.getIndex(), JudiColumn.OOP_ONC_AMOUNT.getIndex()},
-            PrimeColumn.OUT_OF_POCKET_ACCUM_CODE.getIndex(),
-            amountColumnIndex
-        ),
-        new PairMapper(
-            new int[]{JudiColumn.OOP_ONC_OON.getIndex(), JudiColumn.OOP_ONC_OON_AMOUNT.getIndex()},
-            PrimeColumn.OUT_OF_POCKET_ACCUM_CODE.getIndex(),
-            amountColumnIndex
-        ),
-        new PairMapper(
-            new int[]{JudiColumn.OOP_SPEC.getIndex(), JudiColumn.OOP_SPEC_AMOUNT.getIndex()},
-            PrimeColumn.OUT_OF_POCKET_ACCUM_CODE.getIndex(),
-            amountColumnIndex
-        )
+        new FunctionalMapper(row -> {
+          int[][] inputIndexesArray = new int[][]{
+              {JudiColumn.OOP_BOTH.getIndex(), JudiColumn.OOP_BOTH_AMOUNT.getIndex()},
+              {JudiColumn.OOP_OON.getIndex(), JudiColumn.OOP_OON_AMOUNT.getIndex()},
+              {JudiColumn.OOP_ONC.getIndex(), JudiColumn.OOP_ONC_AMOUNT.getIndex()},
+              {JudiColumn.OOP_ONC_OON.getIndex(), JudiColumn.OOP_ONC_OON_AMOUNT.getIndex()},
+              {JudiColumn.OOP_SPEC.getIndex(), JudiColumn.OOP_SPEC_AMOUNT.getIndex()}
+          };
+
+          for (int[] inputIndexes : inputIndexesArray) {
+            Optional<List<OutputMapping>> optionalList = getAmountMapping(
+                row,
+                inputIndexes[0], // inputCodeIndex
+                inputIndexes[1], // inputAmountIndex
+                PrimeColumn.DEDUCTIBLE_ACCUM_CODE.getIndex(),
+                amountColumnIndex
+            );
+            if (optionalList.isPresent()) {
+              return optionalList.get();
+            }
+          }
+
+          // Default to nothing if a non-zero value is not found
+          return Collections.emptyList();
+        })
     );
   }
 
@@ -212,8 +148,13 @@ public class ColumnMapperFactory {
         double number = Double.parseDouble(value);
         if (number != 0) {
           return Optional.of(List.of(
+              // Absolute value of amount
               new OutputMapping(outputAmountIndex, String.valueOf(Math.abs(number))),
+
+              // Accum Code
               new OutputMapping(outputCodeIndex, row[inputCodeIndex]),
+
+              // Transaction Sign based on positive/negative amount
               new OutputMapping(PrimeColumn.TRANSACTION_SIGN.getIndex(), number > 0 ? "P" : "X")
           ));
         }
